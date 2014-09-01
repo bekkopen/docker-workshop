73162484cf53c9d79b38e548b6bbfe06c791c38a
Tobias G. Waaler <tobias.waaler@bekk.no>
2014-09-01 18:33:34 +0200

    display a simple page if Node app cannot connect to mongo

--- app/src/index.js
+++ app/src/index.js
@@ -1,41 +1,48 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var port = process.env.MONGODB_PORT_27017_TCP_PORT;
+var mongoPort = process.env.MONGODB_PORT_27017_TCP_PORT;
 var ip = process.env.MONGODB_PORT_27017_TCP_ADDR;
-var connectionString = 'mongodb://'+ip+':'+port+'/local';
-console.log('connecting to mongodb on ' + connectionString);
-mongoose.connect(connectionString);
-
-var Schema = mongoose.Schema;
-
-var Post = mongoose.model('Post', { content: String });
-
-// Constants
 var PORT = 8888;
 
 // App
 var app = express();
 app.use(express.bodyParser());
 
-app.get('/', function (req, res) {
+if(mongoPort === undefined || ip === undefined){
+  app.get('/', function (req, res) {
+    res.send("Running without mongodb");
+  });
+}
 
-  Post.find( function (err, posts) {
-    var list = "<ol>";
-    for (var i = 0; i < posts.length; i++) {
-      list += "<li>" + posts[i].content + "</li>";
-    }
-    list += "</ol>";
-    var output = list + '<form method="post" action="/"><input name="content" type="text"/><button type="submit"></form>';
-    res.send(output);
+else {
+  var connectionString = 'mongodb://'+ip+':'+mongoPort+'/local';
+  console.log('connecting to mongodb on ' + connectionString);
+  mongoose.connect(connectionString);
+
+  var Schema = mongoose.Schema;
+
+  var Post = mongoose.model('Post', { content: String });
+
+  app.get('/', function (req, res) {
+
+    Post.find( function (err, posts) {
+      var list = "<ol>";
+      for (var i = 0; i < posts.length; i++) {
+        list += "<li>" + posts[i].content + "</li>";
+      }
+      list += "</ol>";
+      var output = list + '<form method="post" action="/"><input name="content" type="text"/><button type="submit"></form>';
+      res.send(output);
+    });
+
   });
 
-});
-
-app.post('/', function (req, res) {
-  var post = new Post({ content: req.body.content });
-  post.save();
-  res.redirect('/');
-});
+  app.post('/', function (req, res) {
+    var post = new Post({ content: req.body.content });
+    post.save();
+    res.redirect('/');
+  });
+}
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
